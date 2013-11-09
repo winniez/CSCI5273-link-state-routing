331077d03bf8d7938809403b5af48a338d4bb71d
xinying <xinyingzengvy@gmail.com>
2013-11-09 16:07:53 -0700

    able to connect each other in any order of start up

--- routed_LS.c
+++ routed_LS.c
@@ -5,12 +5,13 @@
 #include<sys/types.h>
 #include<sys/socket.h>
 #include<sys/select.h>
+#include<sys/fcntl.h>
 #include<netinet/in.h>
 #include<netdb.h>
 #include<unistd.h>
+#include <time.h>
 #include"util.h"
 
-#define DEBUG 1
 
 /*
  * ./routed_LS <RouterID> <LogFileName> <Initialization file>
@@ -21,12 +22,14 @@ int main(int argc, char *argv[]){
 
     FILE *config_file;
     FILE *log_file;
+
+    static const char ip_addr[] = "127.0.1.1";
     
     // router id
     char router_id[10];
     strcpy(router_id, argv[1]);
     int rd_buffer_len = 128;
-    int rd_line_len;
+    int rd_line_len = 0;
     char line[rd_buffer_len];
     char *ptrline = &line[0];
 
@@ -39,24 +42,153 @@ int main(int argc, char *argv[]){
     config_file = fopen(argv[3], "r");
     if (!config_file)
     {
-    printf("%s: Failed open configuration file %s \n", argv[0], argv[1]);
-    exit(1);
+        printf("%s: Failed open configuration file %s \n", argv[0], argv[1]);
+        exit(1);
     }
     // read config file
-    do{
-        rd_line_len = getline(&ptrline, &rd_buffer_len, config_file);
-        // printf("%s", line);
+    rd_line_len = getline(&ptrline, &rd_buffer_len, config_file);
+    while(rd_line_len != -1)
+    {
         // take the line read from file to initialize route
         config_router_link(&router, line);
+        rd_line_len = getline(&ptrline, &rd_buffer_len, config_file);
 
-    }while(rd_line_len != -1);
+    }
+    // close configuration file
     fclose(config_file);
+    
     // print router for debug
     print_router(&router);
 
+    // open log file
+    log_file = fopen(argv[2], "w");
+    if (!log_file)
+    {
+        printf("Failed open log file %s for router %s.\n", argv[2], argv[1]);
+        exit(1);
+    }
 
-    
-    
+
+    // create socket and set non-blocking mode
+    int i =0;
+    for (i=0; i<router.link_cnt; i++)
+    {
+        if ((router.links[i].tmp_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+        {
+            printf("%s, %d, %s, %d: cannot open socket.\n", 
+                    router.links[i].dstA, router.links[i].dstA_port, 
+                    router.links[i].dstB, router.links[i].dstB_port);
+        }
+        // remove the non-block setting here
+        // if connect is unblock mode, connect would immediately return failure
+        // when connect request queuing on listening port
+        // Accept is set to nonblock mode and iteratively check all ports
+        
+        // set socket to non-block mode
+        /*
+        if (fcntl(router.links[i].tmp_fd, F_SETFL, O_NDELAY)<0)
+        {
+            printf("%s, %d, %s, %d: cannot set non-block mode socket.\n",
+                    router.links[i].dstA, router.links[i].dstA_port,
+                    router.links[i].dstB, router.links[i].dstB_port);
+        }
+        */
+        // set address
+        bzero(&router.links[i].local_addr, sizeof(router.links[i].local_addr));
+        bzero(&router.links[i].remote_addr, sizeof(router.links[i].local_addr));
+        router.links[i].local_addr.sin_family = AF_INET;
+        router.links[i].remote_addr.sin_family = AF_INET;
+        router.links[i].local_addr.sin_addr.s_addr = INADDR_ANY;
+        router.links[i].remote_addr.sin_addr.s_addr = INADDR_ANY;
+        router.links[i].local_addr.sin_port = htons(router.links[i].dstA_port);
+        router.links[i].remote_addr.sin_port = htons(router.links[i].dstB_port);
+        // set connection flag
+        router.links[i].connected = 0;
+    }
+
+
+    // attempt to connect, if failed create new socket for listening
+    for (i=0; i<router.link_cnt; i++)
+    {
+        if (connect(router.links[i].tmp_fd, (struct sockaddr*)&(router.links[i].remote_addr), 
+                    sizeof(router.links[i].remote_addr)) == 0)
+        {
+            router.links[i].connected = 1;
+            router.links[i].connect_fd = router.links[i].tmp_fd;
+            printf("CONNECT SUCCESS!: %s, %d, %s, %d\n",
+                    router.links[i].dstA, router.links[i].dstA_port,
+                    router.links[i].dstB, router.links[i].dstB_port);
+        }
+        else    // connect failure
+        {
+            router.links[i].connected = 0;
+            printf("CONNECT FAILED!: %s, %d, %s, %d\n",
+                    router.links[i].dstA, router.links[i].dstA_port,
+                    router.links[i].dstB, router.links[i].dstB_port);
+            // create listen socket
+            if ((router.links[i].tmp_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+            {
+                printf("%s, %d, %s, %d: cannot open socket.\n",
+                    router.links[i].dstA, router.links[i].dstA_port,
+                    router.links[i].dstB, router.links[i].dstB_port);
+            }    
+            // bind socket with local address
+            if (bind(router.links[i].tmp_fd, (struct sockaddr*)&(router.links[i].local_addr), 
+                        sizeof(router.links[i].local_addr))<0)
+            {
+                printf("BIND FAILED!: %s, %d, %s, %d\n",
+                        router.links[i].dstA, router.links[i].dstA_port,
+                        router.links[i].dstB, router.links[i].dstB_port);
+                exit(1);
+            }
+
+            // set socket to non-block mode
+            if (fcntl(router.links[i].tmp_fd, F_SETFL, O_NDELAY)<0)
+            {
+                printf("%s, %d, %s, %d: cannot set non-block mode socket.\n",
+                        router.links[i].dstA, router.links[i].dstA_port,
+                        router.links[i].dstB, router.links[i].dstB_port);
+            }
+            // listen on port
+            if (listen(router.links[i].tmp_fd, MAX_DEGREE)<0)
+            {
+                printf("LISTEN FAILED!: %s, %d, %s, %d\n",
+                        router.links[i].dstA, router.links[i].dstA_port,
+                        router.links[i].dstB, router.links[i].dstB_port);
+            }
+        }
+    }
+
+    while(1)
+    {
+        // scan all ports, accept connection on unconnected ports
+        for (i=0; i<router.link_cnt; i++)
+        {
+            if (!router.links[i].connected)
+            {// call accept on unconnected port
+                router.links[i].connect_fd = accept(router.links[i].tmp_fd, NULL, sizeof(struct sockaddr_in));
+                if (router.links[i].connect_fd < 0)
+                {// failed to connect
+
+                }
+                else
+                {// connection established
+                    router.links[i].connected = 1;
+                    printf("CONNECTION ESTABLISHED!: %s, %d, %s, %d\n",
+                        router.links[i].dstA, router.links[i].dstA_port,
+                        router.links[i].dstB, router.links[i].dstB_port);
+                }
+                
+                
+            }
+            else
+            {// connection established, send LSP on port
+                
+            }    
+        }
+    }
+
+
     return 0;
 
 }
--- util.h
+++ util.h
@@ -1,17 +1,40 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/select.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/time.h>
+#include <unistd.h>
+#include <time.h>
 
 #define MAX_DEGREE 10
 #define MAX_NUM_NODES 24
+#define DEBUG 1
+#define TIMEOUT 20
+
+/*
+ * Link struct
+ * Fields for link dstinations, port and socket descriptor and cost
+ */
 typedef struct {
     char dstA[10];
     int dstA_port;
     char dstB[10];
     int dstB_port;
     int cost;
+    int tmp_fd;     // socket for connect or listen
+    int connect_fd; // socket for connection
+    int connected;
+    struct sockaddr_in local_addr, remote_addr;
 } Link;
+
+/*
+ * Router struct
+ * Fields for Router ID, Link array and number of links
+ */
 
 typedef struct {
     char ID[10];
@@ -19,7 +42,25 @@ typedef struct {
     int link_cnt;
 } Router;
 
-void config_router_link(Router* router, char* fline)
+/*
+ * Link State Packet struct
+ * Fields for Router ID, neighbors and link costs
+ */
+
+typedef struct {
+    char ID[10];
+    int Seq;
+} LSP;
+
+
+/*
+ * void config_router_link(Router *router, char *fline)
+ * inputs:  Router *router: pointer to router to be configured
+ *          fline *line: line read from config file
+ * Initialize router links with fline.
+ */
+
+void config_router_link(Router *router, char *fline)
 {
     char *delimiters = "<,>";
     char *pch = NULL;
@@ -31,7 +72,6 @@ void config_router_link(Router* router, char* fline)
         // identify router
         if (strcmp(pch, router->ID) == 0)
         {
-            printf("match %s\n", router->ID);
             link = &(router->links[router->link_cnt]);
             // config dstA_port
             strcpy(link->dstA, pch);
@@ -53,6 +93,11 @@ void config_router_link(Router* router, char* fline)
     }
 }
 
+/*
+ * void print_router(Router *router)
+ * input: Router *router: pointer to router.
+ * print router_id, number of neighbors and links
+ */
 void print_router(Router *router)
 {
     printf("Router ID: %s, num of links %d\n", router->ID, router->link_cnt);
@@ -64,6 +109,56 @@ void print_router(Router *router)
         printf("%s,%d,%s,%d,%d\n", link->dstA, link->dstA_port, link->dstB, link->dstB_port, link->cost);
     }
 }
+
+/*
+ * int timeout_recvfrom
+ * inputs:  int sock: socket descriptor
+ *          void *buf: buffer to receive msg
+ *          int length: length of buffer
+ *          struct sockaddr* connection: address
+ * outpus:  return 1 for successfully received
+ *          return 0 for timeout
+ */
+int timeout_recvfrom (int sock, void *buf, int length, struct sockaddr *connection)
+{
+    // printf("in timeout_recvfrom\n");
+    fd_set socks;
+    // add socket into fd_set
+    FD_ZERO(&socks);
+    FD_SET(sock, &socks);
+    struct timeval t;
+    t.tv_usec = TIMEOUT;
+    int connection_len = sizeof(connection);
+
+    if (select(sock + 1, &socks, NULL, NULL, &t) &&
+            recvfrom(sock, buf, length, 0, (struct sockaddr *)connection, &connection_len)>=0)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+/* 
+ * int timeout_accept
+ * inputs:  
+ *          
+ * output:  return 1 for successfully accept connection
+ *          return 0 for timeout
+ */
+
+
+
+/* 
+ * int timeout_connect
+ * inputs:  
+ *          
+ * outpus:  return 1 for successfully accept connection
+ *          return 0 for timeout
+ *
+ */
 
 
 
